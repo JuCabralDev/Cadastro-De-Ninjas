b5010871893b684af30c0b3ca8a5309a179c9f67
Júlia Cabral Medeiros <julia.cabral.medeiros@gmail.com>
2025-03-20 10:37:44 -0300

    utilizando lombok

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/missoesNinjasModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/missoesNinjasModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Serverce.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class missoesNinjasModel {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Serverce/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Serverce/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas.Controller.Serverce;
 
 import dev.java10x.CadastroDeNinjas.Missoes.missoesNinjasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,6 +12,9 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro") //TB vem de tabela
+@Data // criar os getters e setters
+@NoArgsConstructor // criar um construtor vazio
+@AllArgsConstructor // criar um construtor cheio
 public class NinjaModel {
 
     @Id
@@ -23,36 +29,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreing key ou chave estrangeira
     private missoesNinjasModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
