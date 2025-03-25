5ad74610aa944609bf6c4850707f20c713ae981f
Júlia Cabral Medeiros <julia.cabral.medeiros@gmail.com>
2025-03-25 10:04:06 -0300

    Feat: Inclui a rota de cadastro de ninjas usando metodo POST, tambem inicializa  o postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Serverce/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Serverce/NinjaService.java
@@ -24,4 +24,9 @@ public class NinjaService {
         Optional<NinjaModel> ninjaPorID = ninjaRepository.findById(id);
                 return ninjaPorID.orElse(null);
     }
+
+    // criar um novo ninja
+    public NinjaModel criarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
 }
