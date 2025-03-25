f79d61c7dfe890004b94d6069b1a961a79639baf
Júlia Cabral Medeiros <julia.cabral.medeiros@gmail.com>
2025-03-25 09:03:25 -0300

    Feat: altera a rota do controller para listar os ninjas e injeta a dependencia do service e do repository

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,14 +2,22 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("missoes")
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
 
     // GET -- Mandar uma requicisao para mostrar as missoes
     @GetMapping("/listar")
-        public String listarMissoes() {
-        return "Missao listada com sucesso";
+       public List<missoesNinjasModel> listarmissoes() {
+        return  missoesService.listarMissoes();
     }
     // POST -- Mandar uma requicisao para criar as missoes
     @PostMapping("/criar")
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,20 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Serverce.NinjaModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Listar Missoes
+    public List<missoesNinjasModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
 }
